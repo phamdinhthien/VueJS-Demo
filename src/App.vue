0b3cd82d915721716bfa054e796d832667369321
Thiện Phạm Đình <phamdinhthien99@gmail.com>
2025-11-26 21:21:53 +0700

    x

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app" class="container-fluid">
+    <img :src="logo.imgSrc_1" alt="Logo" />
+    <img :src="logo.imgSrc_2" alt="Logo" />
+    <img :src="logo.imgSrc_3" alt="Logo" />
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      logo: {
+        imgSrc_1: require('./assets/logo.png'),
+        imgSrc_2: require('./assets/logo.png'),
+        imgSrc_3: require('./assets/logo.png'),
+      }
+    }
+  }
 }
 </script>
 
